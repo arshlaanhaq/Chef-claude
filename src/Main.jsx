37a072b57b64432ad60581aa83f3045dd0d9e8ea
Arshlaan haque <arshlaanhaq005@gmail.com>
2025-02-23 23:36:21 +0530

    changes added

--- src/Main.jsx
+++ src/Main.jsx
@@ -6,7 +6,19 @@ import { getRecipeFromMistral } from "./ai"
 export default function Main() {
     const [ingredients, setIngredients] = React.useState([])
     const [recipe, setRecipe] = React.useState("")
-    
+    const recipeSection = React.useRef(null)
+
+    React.useEffect(() => {
+        if (recipe != "" && recipeSection.current != null) {
+            // recipeSection.current.scrollIntoView({ behavior: "smooth" })
+            const yCoord = recipeSection.current.getBoundingClientRect().top + window.scrollY
+            window.scroll({
+                top: yCoord,
+                behavior: "smooth"
+            })
+        }
+    }, [recipe])
+
     async function getRecipe() {
         const recipeMarkdown = await getRecipeFromMistral(ingredients)
         setRecipe(recipeMarkdown)
@@ -38,6 +50,7 @@ export default function Main() {
 
             {ingredients.length > 0 &&
                 <IngredientsList
+                    ref={recipeSection}
                     ingredients={ingredients}
                     getRecipe={getRecipe}
                 />
